dff4e37aa5b2f7eb5d094c20af7039c3fc70ab0c
jhasanjiv5@gmail.com <jhasanjiv5@gmail.com>
2020-01-08 20:18:06 +0100

    config added

---
+++ config.py
@@ -0,0 +1,14 @@
+
+frozenInferenceGraph= r'pre-trained-set/frozen_inference_graph.pb'
+trainNodeDescription= r'pre-trained-set/output.pbtxt'
+
+rawImagePath = r'images/61-402layout.png'
+
+windowSize= (8,8)
+
+imageResolution= (720, 1080)
+faceClassifierAddress= r'./Downloads/opencv-3.4/data//haarcascades/haarcascade_frontalface_default.xml'
+captureDuration = 120
+sleepDuration = 3600
+
+outputPath= r'output/'
