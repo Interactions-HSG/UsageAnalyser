4a97573e6042c7b6725b65c362edf0406d7c22a0
jhasanjiv5@gmail.com <jhasanjiv5@gmail.com>
2020-10-08 20:26:17 +0200

    requirements list added

---
+++ requirements-pi-aptget.sh
@@ -0,0 +1,27 @@
+sudo apt-get install libjasper-dev
+
+sudo apt install libqtgui4
+
+sudo apt-get install libqt4-test
+
+sudo apt-get install libhdf5-serial-dev
+
+sudo apt-get install libhdf5-dev
+
+sudo apt-get install libatlas-base-dev
+
+pip install opencv-python
+
+pip install opencv-contrib-python==4.1.0.25
+
+pip install imutils
+
+pip install upgrade httplib2==0.15.0
+
+pip install --upgrade google-api-python-client
+
+pip install google-auth-oauthlib
+
+pip install matplotlib
+
+pip install picamera
